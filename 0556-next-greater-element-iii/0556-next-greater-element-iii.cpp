e02f7dbfab85d6d44d7a9c1a4ed291928733436e
Nguyen Kim Thanh <nkimthanh47@gmail.com>
2026-07-26 20:43:53 +0700

    Time: 0 ms (100%), Space: 7.9 MB (24.79%) - LeetHub

---
+++ 0556-next-greater-element-iii/0556-next-greater-element-iii.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int nextGreaterElement(int n) {
+        string s = to_string(n);
+        if(next_permutation(s.begin(), s.end())){
+            long long ans = stoll(s);
+            if(ans > INT_MAX)
+                return -1;
+            return ans;
+        }
+        return -1;
+    }
+};
