2120c5b899697b77038d207e202e6ae81afd5bf7
Nguyen Kim Thanh <nkimthanh47@gmail.com>
2026-09-17 20:27:38 +0700

    Time: 0 ms (100%), Space: 11.7 MB (73.81%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        vector<int> check(26, 0);
+        vector<int> MyChar(26, 0);
         for(int i = 0; i < magazine.length(); i++){
             char c = magazine[i];
-            check[c - 'a']++;
+            MyChar[c - 'a']++;
         }
         for(int i = 0; i < ransomNote.length(); i++){
             char c = ransomNote[i];
-            check[c - 'a']--;
-            if(check[c - 'a'] < 0)
+            MyChar[c - 'a']--;
+            if(MyChar[c - 'a'] < 0){
                 return false;
+            }
         }
         return true;
     }
